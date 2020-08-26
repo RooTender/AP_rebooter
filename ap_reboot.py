b72b8b8bf54b10847df806c401a7a47661eff650
Hubert <lewandowski.hubert486@gmail.com>
2020-08-26 10:11:30 +0200

    First release

--- ap_reboot.py
+++ ap_reboot.py
@@ -11,9 +11,7 @@ def getElement(elementName, by, timeout = 10):
         )
     except:
         print("Timeout {0} seconds passed!".format(timeout))
-        return 0
-
-
+        return None
 
 PATH = r"C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
@@ -21,7 +19,23 @@ driver = webdriver.Chrome(PATH)
 driver.get("http://192.168.0.1/")
 
 loginInput = getElement("loginPassword", By.ID)
+if loginInput is None:
+    driver.refresh()
+    loginInput = getElement("loginPassword", By.ID)
+
 loginInput.send_keys("82458393")
 loginInput.send_keys(Keys.RETURN)
+del loginInput
+
+elementToClick = getElement("c_mu25", By.ID)
+elementToClick.click()
+elementToClick = getElement("c_mu27", By.ID)
+elementToClick.click()
+elementToClick = getElement("c_rr14", By.ID)
+elementToClick.click()
+elementToClick = getElement("c_st28", By.ID)
+elementToClick.click()
+
+print("Success!")
 
 driver.quit()
