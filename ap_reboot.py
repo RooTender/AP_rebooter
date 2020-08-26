c5b3b928392aa23e3d8e790732a73306ddc90fe5
Hubert <lewandowski.hubert486@gmail.com>
2020-08-26 09:53:34 +0200

    Automated logging

--- ap_reboot.py
+++ ap_reboot.py
@@ -1,7 +1,27 @@
 from selenium import webdriver
-PATH = r"C:\Program Files (x86)\chromedriver.exe"
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 
+def getElement(elementName, by, timeout = 10):
+    try:
+        return WebDriverWait(driver, timeout).until(
+            EC.element_to_be_clickable((by, elementName))
+        )
+    except:
+        print("Timeout {0} seconds passed!".format(timeout))
+        return 0
+
+
+
+PATH = r"C:\Program Files (x86)\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
 
-driver.get("https://www.google.pl/")
-driver.close()
+driver.get("http://192.168.0.1/")
+
+loginInput = getElement("loginPassword", By.ID)
+loginInput.send_keys("82458393")
+loginInput.send_keys(Keys.RETURN)
+
+driver.quit()
