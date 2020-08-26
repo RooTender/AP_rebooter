dd1af2f150bf63694a8125cea3c67089ea618442
Hubert <lewandowski.hubert486@gmail.com>
2020-08-26 10:35:51 +0200

    Added dynamic password handler

--- ap_reboot.py
+++ ap_reboot.py
@@ -1,8 +1,27 @@
+from pathlib import Path
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+
+passwordFilePath = "./routerPass.txt"
+
+def checkIfPasswordExist():
+    pathToPassFile = Path(passwordFilePath)
+    
+    if not pathToPassFile.exists():
+        print("Please type password to router login page: ", end="")
+        password = input()
+
+        pathToPassFile.touch()
+        del pathToPassFile
+
+        passwordFile = open(passwordFilePath, "w")
+        passwordFile.write(password)
+        passwordFile.close()
+    else:
+        print("Password file found!")
 
 def getElement(elementName, by, timeout = 10):
     try:
@@ -13,9 +32,9 @@ def getElement(elementName, by, timeout = 10):
         print("Timeout {0} seconds passed!".format(timeout))
         return None
 
-PATH = r"C:\Program Files (x86)\chromedriver.exe"
-driver = webdriver.Chrome(PATH)
+checkIfPasswordExist()
 
+driver = webdriver.Chrome(r"C:\Program Files (x86)\chromedriver.exe")
 driver.get("http://192.168.0.1/")
 
 loginInput = getElement("loginPassword", By.ID)
@@ -23,18 +42,18 @@ if loginInput is None:
     driver.refresh()
     loginInput = getElement("loginPassword", By.ID)
 
-loginInput.send_keys("82458393")
+passwordFile = open(passwordFilePath, "r")
+
+loginInput.send_keys(passwordFile.read())
 loginInput.send_keys(Keys.RETURN)
 del loginInput
 
-elementToClick = getElement("c_mu25", By.ID)
-elementToClick.click()
-elementToClick = getElement("c_mu27", By.ID)
-elementToClick.click()
-elementToClick = getElement("c_rr14", By.ID)
-elementToClick.click()
-elementToClick = getElement("c_st28", By.ID)
-elementToClick.click()
+passwordFile.close()
+
+elements = ("c_mu25", "c_mu27")#, "c_rr14", "c_st28")
+for element in elements:
+    elementToClick = getElement(element, By.ID)
+    elementToClick.click()
 
 print("Success!")
 
